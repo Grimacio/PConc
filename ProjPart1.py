e22cdf1680d61ea48fefa65eb49626d8c39f9683
Guigaspar26 <guilhermebastosgaspar@gmail.com>
2022-12-10 19:49:01 +0000

    checkpoint

--- ProjPart1.py
+++ ProjPart1.py
@@ -1,4 +1,32 @@
 from threading import Thread
-from time import time
+from PIL import Image
 
-print("Hello World 2")
+def resize():
+
+    return
+
+def watermark(imagem):
+    watermark= Image.open("watermark.png")
+    imagem=Image.open(imagem)
+    # image watermark
+    size = (500, 100)
+    crop_image = watermark.copy()
+    crop_image.thumbnail(size)
+    
+    # add watermark
+    copied_image = imagem.copy()
+    copied_image.paste(crop_image, (500, 200))
+    copied_image.show()
+    return copied_image
+
+lista=[]
+new=[]
+
+f= open("nomes.txt", "r")
+
+with f as texto:
+    for line in texto:
+        lista+=[line.replace("\n", "")]
+
+
+watermark(lista[0])
