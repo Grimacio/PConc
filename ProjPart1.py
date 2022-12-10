b3cec514ecb9b90df743345e93b97985cfc8e494
Guigaspar26 <guilhermebastosgaspar@gmail.com>
2022-12-10 20:36:42 +0000

    meio que alguma coisa

--- ProjPart1.py
+++ ProjPart1.py
@@ -1,22 +1,21 @@
 from threading import Thread
 from PIL import Image
 
-def resize():
-
-    return
+def resize(imagem, dimensoes):
+    copy= Image.open(imagem)
+    copy=copy.resize(dimensoes)
+    return copy
 
 def watermark(imagem):
     watermark= Image.open("watermark.png")
     imagem=Image.open(imagem)
     # image watermark
-    size = (500, 100)
+    size = (250, 50)
     crop_image = watermark.copy()
-    crop_image.thumbnail(size)
     
     # add watermark
     copied_image = imagem.copy()
-    copied_image.paste(crop_image, (500, 200))
-    copied_image.show()
+    copied_image.paste(crop_image, size)
     return copied_image
 
 lista=[]
@@ -28,5 +27,7 @@ with f as texto:
     for line in texto:
         lista+=[line.replace("\n", "")]
 
-
-watermark(lista[0])
+watermark(lista[0]).show()
+'''imagem=Image.open(lista[0])
+imagem.thumbnail((150,150))
+imagem.save(lista[0].split(".")[0]+ ".thumbnail", "JPEG")'''
